3c295fc1ab2948814eb0cc95f5ef3c4ac178029d
viswanathan <viswanathan.nachiappan.1@gmail.com>
2026-08-04 12:32:06 +0100

    created camel app to support  MQTT5 with mTLS using paho-mqtt5

--- src/main/java/com/arkmq/PipelineRoute.java
+++ src/main/java/com/arkmq/PipelineRoute.java
@@ -7,22 +7,28 @@ import java.util.concurrent.atomic.AtomicInteger;
 @ApplicationScoped
 public class PipelineRoute extends RouteBuilder {
 
-
     private final AtomicInteger messageCount = new AtomicInteger(0);
 
     @Override
     public void configure() throws Exception {
 
-        // Automatically creates 15,000 test messages (10 per second) and stops
+        // Evaluate the environment variable in Java
+        String appRole = System.getenv("APP_ROLE");
+        // Update the below string 'producer/consumer' as per YAML
+        boolean isProducer = "producer".equalsIgnoreCase(appRole);
+        boolean isConsumer = "consumer".equalsIgnoreCase(appRole);
+
+        // PRODUCER ROLE: Only runs if APP_ROLE=producer
         from("timer:generator?period=100&repeatCount=15000")
                 .routeId("test-data-generator")
+                .autoStartup(isProducer)
                 .setBody(constant("Auto-generated Test Order"))
-                // Drop it into the start of the pipeline (ORDERS.NEW)
-                .to("jms:queue:{{consumer.queue}}");
+                .to("paho-mqtt5:{{consumer.queue}}");
 
-        from("jms:queue:{{consumer.queue}}?concurrentConsumers=5")
+        // CONSUMER ROLE: Only runs if APP_ROLE=consumer
+        from("paho-mqtt5:{{consumer.queue}}")
                 .routeId("updated-app")
-
+                .autoStartup(isConsumer)
                 .process(exchange -> {
                     int currentCount = messageCount.incrementAndGet();
                     String originalBody = exchange.getIn().getBody(String.class);
@@ -30,14 +36,12 @@ public class PipelineRoute extends RouteBuilder {
                     String processedBody = originalBody + " -> [PROCESSED BY APP]";
                     exchange.getIn().setBody(processedBody);
 
-                    // Log progress
                     if (currentCount % 100 == 0) {
                         log.info("Successfully bridged {} orders...", currentCount);
                     }
                 })
                 .log("About to send to {{producer.queue}}: ${body}")
-                // Drop the finished order into the end of the pipeline (ORDERS.PROCESSED)
-                .to("jms:queue:{{producer.queue}}")
+                .to("paho-mqtt5:{{producer.queue}}")
                 .log("Successfully sent to {{producer.queue}}");
     }
 }
